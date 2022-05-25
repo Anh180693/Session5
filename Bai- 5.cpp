1099d677bb177b7c80e58a6dd0b089d551c2992d
Anh180693 <anhltth2203042@fpt.edu.vn>
2022-05-25 23:24:10 +0700

    Session5

--- Bai- 5.cpp
+++ Bai- 5.cpp
@@ -3,15 +3,19 @@ int main(){
 	int n;
 	printf("Nhap so phan tu mang : ");
 	scanf("%d",&n);
-	int ary[n],i,j;
-	printf("Nhap cac phan tu cua mang : ");
-	for(i=0;i<n;i++){
+	int ary[n];
+	for(int i=0;i<n;i++){
+		printf("Nhap cac phan tu mang : ");
 		scanf("%d",&ary[i]);
-	for(j=0;j<i;j++){		
-		while(ary[i]==ary[j]){
-		printf("Nhap lai phan tu trung nhau :");
-		scanf("%d",&ary[i]);
+		for(int j=0;j<i;j++){
+			while(ary[j]==ary[i]){
+				printf("Nhap lai : ");
+				scanf("%d",&ary[i]);
+			}
 		}
-	}	
+	}
+	
+	for(int i=0;i<n;i++){
+		printf("%d  ",ary[i]);
 	}
 }
